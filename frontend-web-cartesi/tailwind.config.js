5cc3928dff29ace1129961a122dcb793ebff2778
Aaron Bruce <aaronbruce555@gmail.com>
2024-02-27 14:58:45 -0700

    Add a basic theme to tailwind

--- frontend-web-cartesi/tailwind.config.js
+++ frontend-web-cartesi/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
   },
   plugins: [require("daisyui")],
 }
